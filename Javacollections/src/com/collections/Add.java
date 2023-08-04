5364051fc56178f800c753d841b52fea40539e8a
Vignesh kumar.S <89632000+Vignesh-Kumar-svk@users.noreply.github.com>
2023-08-04 10:59:41 +0530

    Add files via upload

---
+++ Javacollections/src/com/collections/Add.java
@@ -0,0 +1,12 @@
+package com.collections;
+import java.util.*;
+
+public class Add {
+	public static void main(String args[]) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("Red");
+		list.add("Green");
+		System.out.println(list);
+	}
+
+}
